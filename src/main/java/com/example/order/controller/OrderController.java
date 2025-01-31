8b16a57ed59fe26cbf0b50813c624b37645e0238
Amauri Tavares <amauri_tavares@hotmail.com>
2025-01-31 10:16:55 -0300

    add documentação no controller

--- src/main/java/com/example/order/controller/OrderController.java
+++ src/main/java/com/example/order/controller/OrderController.java
@@ -9,12 +9,16 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// Este controller é para fins de testes, não será utilizado no projeto final.
+// Ele é responsável por expor os endpoints para a criação e consulta de pedidos no order-service.
+// O serviço externo A que será responsável por se comunicar com o RabbitMQ do order-service e o serviço externo B que será responsável por se comunicar com o banco de dados.
+
+
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
 
     private final OrderService orderService;
-
     private final OrderRepository orderRepository;
 
     public OrderController(OrderService orderService, OrderRepository paymentRepository) {
